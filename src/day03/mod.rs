99f00838c7c666c18e33cdd6627d42f81e5b6f5b
Shane Poppleton <shane@bjja.com.au>
2023-06-15 19:26:42 +1000

    Finished day 3

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -6,3 +6,16 @@ use crate::{Output, Part};
 use input::{Item, ItemSet, Rucksack};
 
 pub type Input = Vec<Rucksack>;
+
+pub fn run(part: Part) -> Output {
+    let input = input::read();
+    match part {
+        Part::One => part1::solve(&input),
+        Part::Two => part2::solve(&input),
+    }
+}
+
+pub fn run_both() -> (Output, Output) {
+    let input = input::read();
+    (part1::solve(&input), part2::solve(&input))
+}
